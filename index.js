eeba6f32c9acda2a04d75890729d9f442f91945e
Aleks Vino <aleks.vino@gmail.com>
2018-01-20 07:26:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   Object[key] = value;
   return object; 
 }
- 
+
+function deleteFromObjectByKey(object, key) {
+var newObj = Object.assign({}, object);
+delete newObj[key];
+return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+delete object[key];
+return object;
+} 
