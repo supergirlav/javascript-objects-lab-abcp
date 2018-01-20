5b8dcc1a81c3c2453e4deb8fef4d7125e14ffa91
Aleks Vino <aleks.vino@gmail.com>
2018-01-20 07:27:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  Object[key] = value;
+  object[key] = value;
   return object; 
 }
 
